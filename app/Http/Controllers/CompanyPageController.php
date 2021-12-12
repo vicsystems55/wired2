11db5e2c453c0a5e5a91bd746f6a5730361dac0a
vicSystems <victorasuquob@gmail.com>
2021-12-12 20:02:27 +0100

    scafolding

---
+++ app/Http/Controllers/CompanyPageController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CompanyPageController extends Controller
+{
+    //
+
+    public function home()
+    {
+        
+        
+
+        return view('company.home');
+    }
+}
